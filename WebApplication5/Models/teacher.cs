aa8164302ada763cdc3ebe69db6cd7362968bd99
春天 <chuntian1983@163.com>
2017-09-29 14:06:58 +0800

    207-9-29

--- WebApplication5/Models/teacher.cs
+++ WebApplication5/Models/teacher.cs
@@ -13,10 +13,13 @@ namespace WebApplication5.Models
         [Required]
 
         [StringLength(200)]
+        [Display(Name ="姓名")]
         public string strname { get; set; }
         [Required]
         [StringLength(200)]
+        [Display(Name ="电话")]
         public string tel { get; set; }
+        [Display(Name ="创建时间")]
         public DateTime creattime { get; set; }
     }
 }
